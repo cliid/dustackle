aafb7347be76f0e5679a8941ae03b0d03b157b69
cliid <cliid@protonmail.com>
2021-12-04 21:23:39 +0900

    Fixed bug where tsconfig.json doesn't accept 'include' which is out of the rootDir.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'dustackle',
-      script: './dist/main.js',
+      script: './dist/src/main.js',
       interpreter_args: '-r ts-node/register/transpile-only -r tsconfig-paths/register',
       instances: 'max',
       exec_mode: 'cluster',
